2e5e240b4f148aade79a0b6aecebbf7bcbaa65a5
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-06-24 10:31:54 -0700

    initial commit

---
+++ src/main/java/tgreenidge/com/recipe_app/recipes/RecipesApplication.java
@@ -0,0 +1,13 @@
+package tgreenidge.com.recipe_app.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tgreenidge/com/recipe_app/recipes/RecipesApplicationTests.java
@@ -0,0 +1,16 @@
+package tgreenidge.com.recipe_app.recipes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
