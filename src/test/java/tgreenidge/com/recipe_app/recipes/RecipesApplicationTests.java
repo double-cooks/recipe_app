693eb65b40595daa2a0e883a121cc67ff251f88e
chidrome <chidrome@gmail.com>
2019-06-27 10:52:07 -0700

    added integration testing for home, login, logout

--- src/test/java/tgreenidge/com/recipe_app/recipes/RecipesApplicationTests.java
+++ src/test/java/tgreenidge/com/recipe_app/recipes/RecipesApplicationTests.java
@@ -1,17 +1,27 @@
 package tgreenidge.com.recipe_app.recipes;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tgreenidge.com.recipe_app.recipes.controllers.AppUserController;
 import tgreenidge.com.recipe_app.recipes.models.AppUser;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -20,7 +30,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class RecipesApplicationTests {
 
 	@Autowired
+	AppUserController appUserControllerTest;
+
+	@Autowired
 	private MockMvc mockMvc;
+
+
 
 	// Helper Methods
 	public AppUser createTestUser(){
@@ -28,7 +43,34 @@ public class RecipesApplicationTests {
 		return testUser;
 	}
 
-	// Route Testing
+
+	// Unit/Integration Testing
+	@Test
+	public void testControllerIsAutowired() {
+		assertNotNull(appUserControllerTest);
+	}
+
+	@Test
+	public void testRequestToRootGivesWelcome() throws Exception {
+		mockMvc.perform(get("/")).andExpect(content().string(containsString("Welcome to Doubly")));
+	}
+
+	@Test
+	public void testRequestToLogoutGivesSuccess() throws Exception {
+		mockMvc.perform(get("/logoutSuccess")).andExpect(content().string(containsString("Logout success!")));
+	}
+
+	@Test
+	public void testRequestToLogInGivesUsername() throws Exception {
+		mockMvc.perform(get("/login")).andExpect(content().string(containsString("Username")));
+	}
+
+	@Test
+	public void testRequestToSignUpGivesUsername() throws Exception {
+		mockMvc.perform(get("/login")).andExpect(content().string(containsString("Username")));
+	}
+	
+
 	@Test
 	public void testSlashNotSignedInRoutePass() throws Exception {
 		mockMvc.perform(get("/")).andExpect(status().isOk());
