8deb0336a2c6e61600bad38c0f69262f88491727
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-06-26 20:14:20 -0700

    add preptime and cooktime routes for a recipe by title for alexa

--- src/main/java/tgreenidge/com/recipe_app/recipes/controllers/AlexaController.java
+++ src/main/java/tgreenidge/com/recipe_app/recipes/controllers/AlexaController.java
@@ -36,7 +36,7 @@ public class AlexaController {
     }
 
     /*
-        Returns all the recipes that are created in the app
+        Returns the first recipe that is created in the app with a given title
      */
     @RequestMapping(value = "/alexa/recipes/{title}", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<List<Recipe>> getAlexaRecipesByTitle(@PathVariable String title) {
@@ -44,6 +44,28 @@ public class AlexaController {
         //get the first recipe with that title
         Recipe recipe = recipes.get(0);
         return new ResponseEntity<List<Recipe>>(recipes, HttpStatus.OK);
+    }
+
+    /*
+        Returns the cooktime that are created in the app
+     */
+    @RequestMapping(value = "/alexa/recipes/{title}/cooktime", method = RequestMethod.GET)
+    public @ResponseBody String getAlexaRecipeCookTimeByTitle(@PathVariable String title) {
+        List<Recipe> recipes = recipeRepository.findByTitle(title.toLowerCase());
+        //get the first recipe with that title
+        Recipe recipe = recipes.get(0);
+        return recipe.getCookTime();
+    }
+
+    /*
+        Returns the preptime that are created in the app
+     */
+    @RequestMapping(value = "/alexa/recipes/{title}/preptime", method = RequestMethod.GET)
+    public @ResponseBody String getAlexaRecipePrepTimeByTitle(@PathVariable String title) {
+        List<Recipe> recipes = recipeRepository.findByTitle(title.toLowerCase());
+        //get the first recipe with that title
+        Recipe recipe = recipes.get(0);
+        return recipe.getPrepTime();
     }
 
     /*
